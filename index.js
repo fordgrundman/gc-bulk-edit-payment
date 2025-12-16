88cce16e5e27083b55023d598fc1e0fbe43a0890
Ford <codingasford@gmail.com>
2025-12-16 15:17:31 -0800

    server-updates and imported stripe

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
+import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
+import Stripe from "stripe";
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
 
-//middleware
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-//health check route
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.post("/create-checkout", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      line_items: [{ price: "price_1Sf6FOJguShk9RUdUS5e2XyS", quantity: 1 }],
+      success_url: "https://www.gcbulkedit.dev/payment-success",
+      cancel_url: "https://www.gcbulkedit.dev/payment-cancel",
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Stripe checkout creation failed" });
+  }
 });
+
+app.get("/", (req, res) => res.send("Server running 🚀"));
 
 //start server
 const PORT = process.env.PORT || 3000;
